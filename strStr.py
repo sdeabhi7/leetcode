40281416f95172847f582a8c68e3b5a5949fc03a
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 <sdeabhi@gmail.com>
2025-04-11 00:12:33 -0700

    leetcode 28

--- strStr.py
+++ strStr.py
@@ -4,8 +4,7 @@ Given two strings needle and haystack, return the index of the first occurrence 
 Example 1:
 Input: haystack = "sadbutsad", needle = "sad"
 Output: 0
-Explanation: "sad" occurs at index 0 and 6.
-The first occurrence is at index 0, so we return 0.
+Explanation: "sad" occurs at index 0 and 6. The first occurrence is at index 0, so we return 0.
 
 Example 2:
 Input: haystack = "leetcode", needle = "leeto"
@@ -13,14 +12,11 @@ Output: -1
 Explanation: "leeto" did not occur in "leetcode", so we return -1.
 
 Constraints:
-1 <= haystack.length, needle.length <= 104
+1 <= haystack.length, needle.length <= 10**4
 haystack and needle consist of only lowercase English characters.
 '''
 
 def strStr(haystack, needle):
-    if needle in haystack:
-        return haystack.index(needle)
-    else:
-        return -1
+    return haystack.index(needle) if needle in haystack else -1 
     
 print(strStr('leetcode', 'leet'))

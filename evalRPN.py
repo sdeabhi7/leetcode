61abfe11b6b279afb81474e4f4b8c2e91a8c49ee
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 <sdeabhi@gmail.com>
2025-06-02 02:44:21 -0700

    leetcode 150

---
+++ evalRPN.py
@@ -0,0 +1,62 @@
+'''
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+The valid operators are '+', '-', '*', and '/'.
+Each operand may be an integer or another expression.
+The division between two integers always truncates toward zero.
+There will not be any division by zero.
+The input represents a valid arithmetic expression in a reverse polish notation.
+The answer and all the intermediate calculations can be represented in a 32-bit integer.
+ 
+Example 1:
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+
+Example 2:
+Input: tokens = ["4","13","5","/","+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+
+Example 3:
+Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+Output: 22
+Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+
+Constraints:
+1 <= tokens.length <= 10**4
+tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+'''
+
+from math import ceil, floor
+def evalRPN(tokens):
+    value = []
+    for i in tokens:
+        if i in '+*-/':
+            m, n = value.pop(), value.pop()
+            if i == '+':
+                value.append(m + n)
+            elif i == '*':
+                value.append(m * n)
+            elif i == '-':
+                value.append(n - m)
+            else:
+                division = n / m
+                if division < 0:
+                    value.append(ceil(division))
+                else:
+                    value.append(floor(division))
+        else:
+            value.append(int(i))
+    return value[0]
+
+print(evalRPN(["2","1","+","3","*"]))

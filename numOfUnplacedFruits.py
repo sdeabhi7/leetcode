48d322700e00772a3be25ea96133b1411a76f66e
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 <sdeabhi@gmail.com>
2025-08-07 10:34:01 -0700

    leetcode 3477

--- numOfUnplacedFruits.py
+++ numOfUnplacedFruits.py
@@ -15,7 +15,7 @@ Explanation:
 fruits[0] = 4 is placed in baskets[1] = 5.
 fruits[1] = 2 is placed in baskets[0] = 3.
 fruits[2] = 5 cannot be placed in baskets[2] = 4.
-Since one fruit type remains unplaced, we return 1.
+Since one fruit type remains unplaced, we return 1
 
 Example 2:
 Input: fruits = [3,6,1], baskets = [6,4,7]

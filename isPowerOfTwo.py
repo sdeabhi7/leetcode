69c9ca7e57288d6a7f8d418bfd3639baa75337a4
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 <sdeabhi@gmail.com>
2025-08-09 09:54:30 -0700

    leetcode 231

---
+++ isPowerOfTwo.py
@@ -0,0 +1,40 @@
+'''
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+Example 1:
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+
+Example 2:
+Input: n = 16
+Output: true
+Explanation: 24 = 16
+
+Example 3:
+Input: n = 3
+Output: false
+ 
+Constraints:
+-2**31 <= n <= 2**31 - 1
+'''
+
+def isPowerOfTwo(n):
+    if n <= 0:
+        return False
+    while n % 2 == 0:
+        n //= 2
+    return n == 1
+
+print(isPowerOfTwo(1))
+
+# def isPowerOfTwo(self, n: int) -> bool:
+    # if n == 1:
+    #     return True
+    # else:
+    #     for i in range(1, int(n/2) + 1):
+    #         if 2 ** i == n:
+    #             return True
+    #     return False

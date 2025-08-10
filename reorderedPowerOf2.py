b9ff744484b1013565b8800e35417fb4e16c99f1
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 <sdeabhi@gmail.com>
2025-08-10 09:06:04 -0700

    leetcode 869

---
+++ reorderedPowerOf2.py
@@ -0,0 +1,21 @@
+'''
+You are given an integer n. We reorder the digits in any order (including the original order) such that the leading digit is not zero.
+
+Return true if and only if we can do this so that the resulting number is a power of two.
+
+Example 1:
+Input: n = 1
+Output: true
+
+Example 2:
+Input: n = 10
+Output: false
+ 
+Constraints:
+1 <= n <= 10**9
+'''
+
+def reorderedPowerOf2(n):
+        return sorted(str(n)) in (sorted(str(1<<i)) for i in range(50))
+
+print(reorderedPowerOf2(1))

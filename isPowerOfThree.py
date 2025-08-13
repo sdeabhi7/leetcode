be5af7cd19c70bd9351894f14ced3dd5f0229bb3
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 <sdeabhi@gmail.com>
2025-08-13 04:08:57 -0700

    leetcode 326

---
+++ isPowerOfThree.py
@@ -0,0 +1,30 @@
+'''
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+Example 1:
+Input: n = 27
+Output: true
+Explanation: 27 = 33
+
+Example 2:
+Input: n = 0
+Output: false
+Explanation: There is no x where 3x = 0.
+
+Example 3:
+Input: n = -1
+Output: false
+Explanation: There is no x where 3x = (-1).
+ 
+Constraints:
+-2**31 <= n <= 2**31 - 1
+'''
+
+def isPowerOfThree(n):
+    while n != 1 and n > 2:
+        n /= 3
+    return True if n == 1 else False
+
+print(isPowerOfThree(27))

b6909cc84f2c57c9ff098901835df25e96ba33d1
sdeabhi7 <sdeabhi@gmail.com>
2025-08-31 21:41:27 +0530

    leetcode 1374

--- generateTheString.py
+++ generateTheString.py
@@ -6,12 +6,14 @@ The returned string must contain only lowercase English letters. If there are mu
 Example 1:
 Input: n = 4
 Output: "pppz"
-Explanation: "pppz" is a valid string since the character 'p' occurs three times and the character 'z' occurs once. Note that there are many other valid strings such as "ohhh" and "love".
+Explanation: "pppz" is a valid string since the character 'p' occurs three times and the character 'z' occurs once. 
+Note that there are many other valid strings such as "ohhh" and "love".
 
 Example 2:
 Input: n = 2
 Output: "xy"
-Explanation: "xy" is a valid string since the characters 'x' and 'y' occur once. Note that there are many other valid strings such as "ag" and "ur".
+Explanation: "xy" is a valid string since the characters 'x' and 'y' occur once. 
+Note that there are many other valid strings such as "ag" and "ur".
 
 Example 3:
 Input: n = 7
@@ -29,3 +31,7 @@ def generateTheString(n):
         return value[0] * (n - 1) + value[1] 
 
 print(generateTheString(4))
+
+# def generateTheString(n):
+#     value = 'abcdefghijklmnopqrstuvwxyz'
+#     return value[0] * n if n % 2 == 1 else value[0] * (n-1) + value[1]

87f90b4decb34c2ca266a767e2bf1eb940874545
abhinav <sdeabhi@gmail.com>
2025-09-24 15:41:13 +0000

    leetcode 3005

--- maxFrequencyElements.py
+++ maxFrequencyElements.py
@@ -12,7 +12,7 @@ Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum fre
 So the number of elements in the array with maximum frequency is 4.
 
 Example 2:
-Input: nums = [1,2,3,4,5
+Input: nums = [1,2,3,4,5]
 Output: 5
 Explanation: All elements of the array have a frequency of 1 which is the maximum.
 So the number of elements in the array with maximum frequency is 5.

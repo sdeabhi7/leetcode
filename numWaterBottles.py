b5646109d900348b6f3ed964db6b87bb1a4828ad
sdeabhi7 <sdeabhi@gmail.com>
2025-10-18 22:10:24 +0530

    leetcode 1518

--- numWaterBottles.py
+++ numWaterBottles.py
@@ -7,7 +7,7 @@ The operation of drinking a full water bottle turns it into an empty bottle.
 
 Given the two integers numBottles and numExchange, return the maximum number of water bottles you can drink.
 
-Example 1:
+Example 1
 Input: numBottles = 9, numExchange = 3
 Output: 13
 Explanation: You can exchange 3 empty bottles to get 1 full water bottle.

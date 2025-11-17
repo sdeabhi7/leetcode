2c25ac6d8710b61c78509153bda79cc18b287863
abhinav <sdeabhi@gmail.com>
2025-11-17 09:50:17 +0000

    leetcode 1437

---
+++ kLengthApart.py
@@ -0,0 +1,30 @@
+'''
+Given an binary array nums and an integer k, return true if all 1's are at least k places away from each other, otherwise return false.
+
+Example 1:
+Input: nums = [1,0,0,0,1,0,0,1], k = 2
+Output: true
+Explanation: Each of the 1s are at least 2 places away from each other.
+
+Example 2:
+Input: nums = [1,0,0,1,0,1], k = 2
+Output: false
+Explanation: The second 1 and third 1 are only one apart from each other.
+ 
+Constraints:
+1 <= nums.length <= 10**5
+0 <= k <= nums.length
+nums[i] is 0 or 1
+'''
+
+def kLengthApart(nums, k):
+    t = []
+    for i in range(len(nums)):
+        if nums[i] == 1:
+            t.append(i)
+    for i in range(1, len(t)):
+        if abs(t[i-1] - t[i]) <= k:
+            return False
+    return True
+
+print(kLengthApart([1,0,0,1,0,1], 2))

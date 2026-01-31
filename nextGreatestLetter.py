90b2e17fd7f01bdf03538f71d64b3302c2681c2c
sdeabhi7 <sdeabhi@gmail.com>
2026-01-31 16:59:48 +0530

    leetcode 744

--- nextGreatestLetter.py
+++ nextGreatestLetter.py
@@ -4,7 +4,7 @@ You are given an array of characters letters that is sorted in non-decreasing or
 Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
 
 Example 1:
-Input: letters = ["c","f","j"], target = ""
+Input: letters = ["c","f","j"], target = "a"
 Output: "c"
 Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
 

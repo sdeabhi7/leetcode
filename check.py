aeb0efcb300469fb124f73b6f9dde52ec4bd7be9
sdeabhi7 <sdeabhi@gmail.com>
2026-05-26 23:03:59 +0530

    leetcode 1752

--- check.py
+++ check.py
@@ -22,9 +22,9 @@ Explanation: [1,2,3] is the original sorted array.
 You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
 
 Constraints:
-
 1 <= nums.length <= 100
-1 <= nums[i] <= 100'''
+1 <= nums[i] <= 100
+'''
 
 #   author: sdeabhi
 

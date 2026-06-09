02c72654828815172534c437a1feab5c59d85b83
sdeabhi7 <sdeabhi@gmail.com>
2026-06-09 20:57:38 +0530

    leetcode 2574

---
+++ leftRightDifference.py
@@ -0,0 +1,42 @@
+'''
+You are given a 0-indexed integer array nums of size n.
+
+Define two arrays leftSum and rightSum where:
+
+leftSum[i] is the sum of elements to the left of the index i in the array nums. If there is no such element, leftSum[i] = 0.
+rightSum[i] is the sum of elements to the right of the index i in the array nums. If there is no such element, rightSum[i] = 0.
+Return an integer array answer of size n where answer[i] = |leftSum[i] - rightSum[i]|.
+
+Example 1:
+Input: nums = [10,4,8,3]
+Output: [15,1,11,22]
+Explanation: The array leftSum is [0,10,14,22] and the array rightSum is [15,11,3,0].
+The array answer is [|0 - 15|,|10 - 11|,|14 - 3|,|22 - 0|] = [15,1,11,22].
+
+Example 2:
+Input: nums = [1]
+Output: [0]
+Explanation: The array leftSum is [0] and the array rightSum is [0].
+The array answer is [|0 - 0|] = [0].
+ 
+Constraints:
+1 <= nums.length <= 1000
+1 <= nums[i] <= 10**5
+'''
+
+def leftRightDifference(nums):
+    n = len(nums)
+    l_sum = [0]
+    r_sum = [0]
+    k = []
+    for i in range(n-1):
+        l_sum.append(l_sum[i] + nums[i])
+    for i in range(n):
+        r_sum.append(r_sum[i] + nums[-i-1])
+    del r_sum[-1]
+    r_sum = r_sum[::-1]
+    for i, j in zip(l_sum, r_sum):
+        k.append(abs(i - j))
+    return k
+    
+print(leftRightDifference([10,4,8,3]))

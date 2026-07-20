64dd23c3243b2bb16ff40c76b47ac956cef1abf4
sdeabhi7 <sdeabhi@gmail.com>
2026-07-20 22:07:37 +0530

    leetcode 1260

---
+++ shiftGrid.py
@@ -0,0 +1,22 @@
+#   author: sdeabhi
+
+
+
+def shiftGrid(grid, k):
+    value = []
+    r = len(grid[0])
+    for i in grid:
+        value.extend(i)
+    n = len(value)
+    y = k % n
+    s = value[n-y:] + value[:n-y]
+    t = []
+    h = []
+    for i in s:
+        h.append(i)
+        if len(h) == r:
+            t.append(h)
+            h = []
+    return t
+
+print(shiftGrid([[1,2,3],[4,5,6],[7,8,9]], 1))

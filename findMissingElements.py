7fa66a4d5c71c50b8be8258f9b40a9fd6af395c5
sdeabhi7 <sdeabhi@gmail.com>
2026-08-04 21:39:34 +0530

    leetcode 3731

---
+++ findMissingElements.py
@@ -0,0 +1,14 @@
+#   author: sdeabhi
+
+
+
+def findMissingElements(nums):
+    value = []
+    min_n = min(nums)
+    max_n = max(nums)
+    for i in range(min_n, max_n):
+        if i not in nums:
+            value.append(i)
+    return value
+
+print(findMissingElements([1,4,2,5]))

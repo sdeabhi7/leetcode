ae376a64b75512e26370dd7cd34c7cb6db66b75e
sdeabhi7 <sdeabhi@gmail.com>
2026-09-08 22:49:31 +0530

    leetcode 3870

---
+++ countCommas.py
@@ -0,0 +1,11 @@
+#   author: sdeabhi
+
+
+
+def countCommas(n):
+    return max(n-999, 0)
+
+print(countCommas(1002))
+
+# def countCommas(n):
+#     return sum(1 for i in range(1000, n+1)) if n >= 1000 else 0

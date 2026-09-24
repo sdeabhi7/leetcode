9d8f5079c9e734ef2e91f6646ef014526ad9176e
sdeabhi7 <sdeabhi@gmail.com>
2026-09-24 21:59:27 +0530

    leetcode 3498

---
+++ reverseDegree.py
@@ -0,0 +1,14 @@
+#   author: sdeabhi
+
+
+
+def reverseDegree(s):
+    y = 'abcdefghijklmnopqrstuvwxyz'
+    r = y[::-1]
+    k = 0
+    t = len(s)
+    for i in range(t):
+        k += (r.find(s[i]) + 1) * (i+1)
+    return k
+
+print(reverseDegree('abc'))
